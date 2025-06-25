27d2c286cd00bdabaa23c7e2127eaf612e49aeb1
SoullessMindful <soullessmindful@gmail.com>
2025-06-25 13:55:26 +0200

    MixNode: updating MixNode to make sure the automation is smooth

--- src/lib/audionodes/MixNode.ts
+++ src/lib/audionodes/MixNode.ts
@@ -1,8 +1,9 @@
 export class MixNode {
   dry: GainNode
   wet: GainNode
-  
+
   private mixSource: ConstantSourceNode
+  private mixGainNode: GainNode
   private flipNode: GainNode
 
   constructor(ctx: BaseAudioContext) {
@@ -10,31 +11,33 @@ export class MixNode {
     this.dry.gain.value = 1
     this.wet = ctx.createGain()
     this.wet.gain.value = 0
-    
+
     this.mixSource = ctx.createConstantSource()
-    this.mixSource.offset.value = 0
+    this.mixSource.offset.value = 1
+    this.mixGainNode = ctx.createGain()
     this.flipNode = ctx.createGain()
     this.flipNode.gain.value = -1
-    
-    this.mixSource.connect(this.wet.gain)
-    this.mixSource.connect(this.flipNode)
+
+    this.mixSource.connect(this.mixGainNode)
+    this.mixGainNode.connect(this.wet.gain)
+    this.mixGainNode.connect(this.flipNode)
     this.flipNode.connect(this.dry.gain)
-    
+
     this.mixSource.start()
   }
-  
+
   connect(node: AudioNode) {
     this.dry.connect(node)
     this.wet.connect(node)
   }
-  
+
   disconnect() {
     this.dry.disconnect()
     this.wet.disconnect()
   }
-  
+
   get mix(): AudioParam {
-    return this.mixSource.offset
+    return this.mixGainNode.gain
   }
 }
 

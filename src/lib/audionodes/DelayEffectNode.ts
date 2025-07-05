88690102a7303e4a236b89bea18b5dc2417d66f0
SoullessMindful <soullessmindful@gmail.com>
2025-07-05 23:11:56 +0200

    DelayEffectNode: adding a delay node to be used later in the global effect chain.

---
+++ src/lib/audionodes/DelayEffectNode.ts
@@ -0,0 +1,50 @@
+import { createMixNode, MixNode } from './MixNode'
+
+export type DelayEffectNodeSettings = {
+  __type__: 'delay'
+  time: AudioParam
+  feedback: AudioParam
+  mix: AudioParam
+}
+
+export class DelayEffectNode {
+  input: GainNode
+  private delayNode: DelayNode
+  private feedbackNode: GainNode
+  private mixNode: MixNode
+
+  constructor(ctx: BaseAudioContext) {
+    this.input = ctx.createGain()
+    this.input.gain.value = 1
+
+    this.delayNode = ctx.createDelay()
+
+    this.feedbackNode = ctx.createGain()
+    this.feedbackNode.gain.value = 0
+
+    this.mixNode = createMixNode(ctx)
+
+    this.input.connect(this.delayNode)
+    this.input.connect(this.mixNode.dry)
+    this.delayNode.connect(this.feedbackNode)
+    this.delayNode.connect(this.mixNode.wet)
+    this.feedbackNode.connect(this.delayNode)
+  }
+
+  connect(node: AudioNode) {
+    this.mixNode.connect(node)
+  }
+
+  disconnect() {
+    this.mixNode.disconnect()
+  }
+
+  get settings(): DelayEffectNodeSettings {
+    return {
+      __type__: 'delay',
+      mix: this.mixNode.mix,
+      time: this.delayNode.delayTime,
+      feedback: this.feedbackNode.gain,
+    }
+  }
+}
